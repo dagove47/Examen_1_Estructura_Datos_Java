392777a03ea9f030b4086598095077dabc008045
alexkun90 <alexander9824@gmail.com>
2022-10-23 11:08:54 -0600

    pila
    ClassPila

--- src/main/java/Examen_1_Estructura_Datos_Java/Pila.java
+++ src/main/java/Examen_1_Estructura_Datos_Java/Pila.java
@@ -9,5 +9,34 @@ package Examen_1_Estructura_Datos_Java;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class Pila {
-    
+    private Nodo topNodo;
+    private int largo = 0;
+    public boolean vacia() {
+        return this.topNodo == null;
+    }
+    public void push() {
+        this.largo++;
+        Nodo newNodo = new Nodo();
+        newNodo.setPlato("Plato " + this.largo);
+        if(this.topNodo == null) {
+            this.topNodo = newNodo;
+        } else {
+            newNodo.setNodoAnterior(this.topNodo);
+            this.topNodo = newNodo;
+        }
+    }
+    public String listarPila(){
+        // Crea una copia de la pila.
+        String listaFinal = "";
+        // Recorre la pila hasta el ultimo node.
+        if(!vacia()) {
+            System.out.println(this.topNodo.getPlato());
+            Nodo aux = this.topNodo;
+            while(aux != null){
+                listaFinal += aux.getPlato() + "\n";
+                aux = aux.getNodoAnterior();
+            }
+        }
+        return listaFinal;
+    }
 }
