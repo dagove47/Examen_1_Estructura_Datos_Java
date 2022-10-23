96d263b089f20f373d6c53ae1ce7527fdad45545
ItisYeWolf17 <105758299+ItisYeWolf17@users.noreply.github.com>
2022-10-23 11:04:05 -0600

    Clase Cola

--- src/main/java/Examen_1_Estructura_Datos_Java/Cola.java
+++ src/main/java/Examen_1_Estructura_Datos_Java/Cola.java
@@ -9,5 +9,53 @@ package Examen_1_Estructura_Datos_Java;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class Cola {
+    private NodoC frente;
+    private NodoC ultimo;
+
+
+
+   public Cola() {
+    }
     
+    public void encola(NodoC newNodo){
+        if(frente == null){
+            frente = newNodo;
+            ultimo = newNodo;
+        } else{
+            ultimo.setNodoAnterior(newNodo);
+            ultimo=newNodo;
+        }
+    }
+    
+    public void preferencial(NodoC newNodo) {
+        if(frente == null){
+            frente = newNodo;
+            ultimo = newNodo;
+        } else{
+            newNodo.setNodoAnterior(frente);
+            frente = newNodo;
+        }
+    }
+
+
+
+   public NodoC atiende(){
+        NodoC aux = frente;
+        if(frente!=null){
+            frente=frente.getNodoAnterior();
+            aux.setNodoAnterior(null);
+        }
+        return aux;
+    }
+    
+    @Override
+    public String toString(){
+        String s="";
+        NodoC aux=frente;
+        while(aux!=null){
+            s+=aux+"\n";
+            aux=aux.getNodoAnterior();
+        }
+        return s;
+    }
 }
